efd8b3ad34f5c3526b80365cf5ef6c3a529058d8
ZackerWins <jgalt@example.com>
2019-02-11 21:40:44 -0600

    second commit

--- Collatz.py
+++ Collatz.py
@@ -12,13 +12,13 @@
 
 
 def collatz_read(s):
-    """
-    read two ints
-    s a string
-    return a list of two ints, representing the beginning and end of a range, [i, j]
-    """
-    a = s.split()
-    return [int(a[0]), int(a[1])]
+	"""
+	read two ints
+	s a string
+	return a list of two ints, representing the beginning and end of a range, [i, j]
+	"""
+	a = s.split()
+	return [int(a[0]), int(a[1])]
 
 # ------------
 # collatz_eval
@@ -26,13 +26,30 @@ def collatz_read(s):
 
 
 def collatz_eval(i, j):
-    """
-    i the beginning of the range, inclusive
-    j the end       of the range, inclusive
-    return the max cycle length of the range [i, j]
-    """
-    # <your code>
-    return 1
+	"""
+	i the beginning of the range, inclusive
+	j the end       of the range, inclusive
+	return the max cycle length of the range [i, j]
+	"""
+	knownValues = []
+
+	for n in range (i,j+1):
+		
+		i = n
+		count = 1
+		if i % 2 == 0:
+			i = (i / 2)
+			count += 1
+		elif i % 2 != 0:
+			i = (i + (i>>1) + 1)
+			count += 2
+		
+		if count not in knownValues:
+			knownValues[n] = count
+
+
+
+	return 1
 
 # -------------
 # collatz_print
@@ -40,14 +57,14 @@ def collatz_eval(i, j):
 
 
 def collatz_print(w, i, j, v):
-    """
-    print three ints
-    w a writer
-    i the beginning of the range, inclusive
-    j the end       of the range, inclusive
-    v the max cycle length
-    """
-    w.write(str(i) + " " + str(j) + " " + str(v) + "\n")
+	"""
+	print three ints
+	w a writer
+	i the beginning of the range, inclusive
+	j the end       of the range, inclusive
+	v the max cycle length
+	"""
+	w.write(str(i) + " " + str(j) + " " + str(v) + "\n")
 
 # -------------
 # collatz_solve
@@ -55,11 +72,11 @@ def collatz_print(w, i, j, v):
 
 
 def collatz_solve(r, w):
-    """
-    r a reader
-    w a writer
-    """
-    for s in r:
-        i, j = collatz_read(s)
-        v = collatz_eval(i, j)
-        collatz_print(w, i, j, v)
+	"""
+	r a reader
+	w a writer
+	"""
+	for s in r:
+		i, j = collatz_read(s)
+		v = collatz_eval(i, j)
+		collatz_print(w, i, j, v)
